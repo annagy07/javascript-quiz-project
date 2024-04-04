ba173ab83d88340c72c5e741fafa1db0fdd1d07b
justArale <just1arale@gmail.com>
2024-04-04 07:36:34 +0200

    merge

--- src/question.js
+++ src/question.js
@@ -1,26 +1,10 @@
 class Question {
-<<<<<<< HEAD
-  // YOUR CODE HERE:
-  //
-  // 1. constructor (text, choices, answer, difficulty)
-=======
->>>>>>> origin
   constructor(text, choices, answer, difficulty) {
     this.text = text;
     this.choices = choices;
     this.answer = answer;
     this.difficulty = difficulty;
   }
-<<<<<<< HEAD
-  shuffleChoices() {
-    let currentIndex = this.choices.length;
-    while (currentIndex != 0) {
-      let randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-
-      [this.choices[currentIndex], this.choices[randomIndex]] = [this.choices[randomIndex],this.choices[currentIndex]];
-    };
-=======
 
   shuffleChoices() {
     let currentIndex = this.choices.length;
@@ -32,6 +16,5 @@ class Question {
         this.choices[currentIndex],
       ];
     }
->>>>>>> origin
   }
 }
